de902f94bbbeba81fb29eb9faf9d340ed372fb3a
Sushant Aryal <sushantaryal05@gmail.com>
2025-06-05 19:51:38 -0500

    edge case note

--- tip101-3a/week 1/threshold_demo.py
+++ tip101-3a/week 1/threshold_demo.py
@@ -1,3 +1,8 @@
+"""
+You are given a list of numbers and a threshold. Your goal is to return an array with numbers in the list that are
+greater than the threshold.
+"""
+
 def above_threshold(lst, threshold):
     #step 1, initialize the list
     result = []
@@ -18,3 +23,10 @@ lst = [8,13,11,4,10,15]
 threshold = 10
 
 print(above_threshold(lst, threshold))
+
+#what if list is empty?
+empty = []
+print(above_threshold(empty, threshold))
+
+# so no need to write any additional code for this edge case :)
+#also don't forget to talk through your process!!
