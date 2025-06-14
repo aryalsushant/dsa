4e8445a8801704cff9530dbf952863a615736807
Sushant Aryal <sushantaryal05@gmail.com>
2025-06-14 15:57:57 -0500

    get highest priority task

--- week2/session2/highest_value.py
+++ week2/session2/highest_value.py
@@ -2,9 +2,30 @@
 
 """
 
-def highest_priority_task(tasks):
-    highest = tasks.value(0)
-    for i in tasks.value():
-        if tasks.value()
+def get_highest_priority_task(tasks):
+    highest_priority = 0
+    best_task = ""
+    for task, priority in tasks.items():
+        if priority > highest_priority:
+            highest_priority = priority
+            best_task = task
+    tasks.pop(best_task)
+    return best_task
+
+#test case
+tasks = {"task1": 8, "task2": 10, "task3": 9, "task4": 10, "task5": 7}
+perform_task = (get_highest_priority_task(tasks))
+print(perform_task)
+
+perform_task = (get_highest_priority_task(tasks))
+print(perform_task)
+
+perform_task = (get_highest_priority_task(tasks))
+print(perform_task)
+
+print(tasks)
+
+
+
 
 
