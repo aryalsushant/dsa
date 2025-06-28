a79630875c92e5e6cf43d85051f1b91471c25c31
Sushant Aryal <sushantaryal05@gmail.com>
2025-06-28 18:58:44 -0500

    squash spaces, i need to get back to it because i dont understand it yet

---
+++ tip101-3a/4 - Two Pointer Technique/session1/squash_spaces.py
@@ -0,0 +1,38 @@
+"""
+The two-pointer approach can also be used with two pointers that iterate forward through a list or string at different 
+rates. Use two pointers to solve the following problem:
+
+Write a function squash_spaces() that takes in a string s as a parameter and returns a new string with each substring 
+with consecutive spaces reduced to a single space. Assume s can contain leading or trailing spaces, but in the result 
+should be trimmed.
+Do not use any of the built-in trim methods.
+"""
+def squash_spaces(s):
+    result = []
+    i = 0
+    n = len(s)
+
+    # Skip leading spaces
+    while i < n and s[i] == ' ':
+        i += 1
+
+    while i < n:
+        if s[i] != ' ':
+            result.append(s[i])
+        else:
+            # Add only one space if the previous char wasn't a space
+            if result and result[-1] != ' ':
+                result.append(' ')
+        i += 1
+
+    # Remove trailing space if present
+    if result and result[-1] == ' ':
+        result.pop()
+
+    return ''.join(result)
+
+
+
+print(squash_spaces("  hello    world  "))
+print(squash_spaces("  what  about  this    ?"))
+print(squash_spaces("this is my sentence"))
