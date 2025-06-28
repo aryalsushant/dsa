07fde5719bcfe7946ad8a6565166d62ccf2499c6
Sushant Aryal <sushantaryal05@gmail.com>
2025-06-28 15:53:38 -0500

    better way to solve perfect number

--- tip101-3a/4 - Two Pointer Technique/session1/perfect_number.py
+++ tip101-3a/4 - Two Pointer Technique/session1/perfect_number.py
@@ -5,12 +5,12 @@ False otherwise. A perfect number is a positive integer that is equal to the sum
 For example, 6 is a perfect number because its divisors or 1, 2, and 3 and 1 + 2 + 3 = 6.
 """
 def is_perfect_number(n):
-    divisors = [1]
+    total = 1
     for i in range(2, n):
         if n%i==0:
-            divisors.append(i)
+            total += i
     
-    return sum(divisors)==n
+    return total == n
 
 
 print(is_perfect_number(6))
