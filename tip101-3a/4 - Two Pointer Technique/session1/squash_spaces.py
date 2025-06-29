e1d03f215015871ff83a5faae5e735aaeaa36c6c
Sushant Aryal <sushantaryal05@gmail.com>
2025-06-28 19:18:54 -0500

    comments for future reference

--- tip101-3a/4 - Two Pointer Technique/session1/squash_spaces.py
+++ tip101-3a/4 - Two Pointer Technique/session1/squash_spaces.py
@@ -33,15 +33,23 @@ def squash_spaces(s):
     return ''.join(result)
 '''
 def squash_spaces(s):
+    #created an empty list to store result
     result = []
-    for i in range(0, len(s)):
+    #looping from 0 to length of s.
+    for i in range(len(s)):
+        #if the character is not space, we append it
         if s[i]!= " ":
             result.append(s[i])
+        #if the character is space but previous character is not space, we append that space
+        #for character at index 0, s[i-1] will be the last character i.e. s[0-1] or s[-1]
+        #so, we add one of the conditions is that i!=0 or i>0
         elif s[i] ==" " and s[i-1]!=" " and i>0:
             result.append(s[i])
-       
+    #at the end, if the last character is a space, we remove it
     if result[-1] == " ":
         result.pop()
+    #convert the resulting array into a string and return it
+    #ppl gonna think this is Ai generated because of well formatted code but no this is just how I work sometimes lol
     return "".join(result)
 
 
