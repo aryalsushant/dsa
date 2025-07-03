c64bd7cb2203ace548119d3e250c70946fc1664c
Sushant Aryal <sushantaryal05@gmail.com>
2025-07-02 21:01:05 -0500

    is caught

--- tip101-3a/5 - Classes and Objects/create_squirtle.py
+++ tip101-3a/5 - Classes and Objects/create_squirtle.py
@@ -5,6 +5,9 @@ Step 2: Instantiate an instance of the class Pokemon and store it in a variable 
 The Pokemon instance created should have name "Squirtle" and its types should be ["Water"].
 
 Step 3: Call the method print_pokemon() on your new Pokemon instance squirtle.
+
+Using your code from Problem 2, update your squirtle Pokemon so that is_caught is updated to True. 
+Use the print_pokemon() function to verify that squirtle's is_caught property was updated.
 """
 class Pokemon:
     def __init__(self, name, types):
@@ -19,4 +22,5 @@ class Pokemon:
             "is_caught": self.is_caught # Output: "is_caught": False
         })
 squirtle = Pokemon("Squirtle", ["Water"])
+squirtle.is_caught = False
 squirtle.print_pokemon()
