a7d273443f106f63695ad4fc733415eb48a86e4a
Sushant Aryal <sushantaryal05@gmail.com>
2025-07-02 20:56:23 -0500

    pokemon class

---
+++ tip101-3a/5 - Classes and Objects/custom_class.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print("hello")
+
+nina_the_student = Student("Nina")
+print(nina_the_student.name)
+        
