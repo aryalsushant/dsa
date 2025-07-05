44b7acd029f74e92671266de4abdf84771aafeda
Sushant Aryal <sushantaryal05@gmail.com>
2025-07-04 22:15:31 -0500

    linked listify

---
+++ tip101-3a/5 - Classes and Objects/session2/linked_listify.py
@@ -0,0 +1,36 @@
+"""
+Write a function list_to_linked_list() that takes in a list lst as a parameter and converts it to a linked list.
+ The function should return the head of the linked list.
+"""
+
+class Node:
+	def __init__(self, value, next=None):
+		self.value = value
+		self.next = next
+		
+def list_to_linked_list(lst):
+	
+	head = Node(lst[0])
+	current = head
+	
+	for nums in lst[1:]:
+		current.next = Node(nums)
+		current = current.next
+	
+	return head
+#helper functipon to print linked list
+def print_linked_list(head):
+	current = head
+	result = []
+	while current is not None:
+		result.append(current.value)
+		current = current.next
+	result.append("None")
+	print (" -> ".join(result))
+
+#test case
+normal_list = ["Betty", "Veronica", "Archie", "Jughead"]
+linked_list = list_to_linked_list(normal_list)
+print_linked_list(linked_list)
+	
+		
