0cb923b02b0047501c4ee79bfe4a5a351ad3e1c6
Sushant Aryal <sushantaryal05@gmail.com>
2025-07-05 16:32:06 -0500

    print card

---
+++ tip101-3a/5 - Classes and Objects/session1/version2/print_card.py
@@ -0,0 +1,25 @@
+"""
+Step 1: Update the Card class with the new method print_card() provided below:
+
+def print_card(self):
+	print(f"{self.rank} of {self.suit}")
+Step 2: Create an instance of the class and store it in a variable named card. 
+The object should have suit "Clubs" and rank "Ace".
+
+Step 3: Then, call the method print_card() on your card.
+
+Expected Output: Ace of Clubs
+"""
+class Card():
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def print_card(self):
+        print(f"{self.rank} of {self.suit}")
+    
+
+card = Card("Clubs", "Ace")
+
+card.print_card()
+
