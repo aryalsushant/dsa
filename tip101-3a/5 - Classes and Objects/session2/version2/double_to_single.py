619981606af6e3d800cb1e6473e82431e00c8bbc
Sushant Aryal <sushantaryal05@gmail.com>
2025-07-06 18:51:32 -0400

    double to single

---
+++ tip101-3a/5 - Classes and Objects/session2/version2/double_to_single.py
@@ -0,0 +1,31 @@
+"""
+Below are the node classes for a singly linked list and a doubly linked list. Write a function dll_to_sll() 
+that takes in the head of a doubly linked list as a parameter and recreates it as a singly linked list.
+
+The function returns the head of the new singly linked list.
+"""
+class SLLNode:
+	def __init__(self, value, next = None):
+		self.value = value
+		self.next = next
+
+class DLLNode:
+	def __init__(self, value, next = None, prev = None):
+		self.value = value
+		self.next = next
+		self.prev = prev
+		
+def dll_to_sll(dll_head):
+	#if linked list is empty then return none
+	if dll_head is None:
+		return None
+	sll_head = SLLNode(dll_head.value)
+	
+	current_sll = sll_head
+	current_dll = dll_head.next
+	while current_dll:
+		new_node = SLLNode(current_dll.value)
+		current_sll.next = new_node
+		current_sll = current_sll.next
+		current_dll=current_dll.next
+	return sll_head
