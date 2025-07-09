1719908fd0e4e88bb7d7481eabadd6324a8aaf74
Sushant Aryal <sushantaryal05@gmail.com>
2025-07-08 21:09:39 -0400

    nested constructor

---
+++ tip101-3a/6 - Advanced Linked List/session1/nested_constructor.py
@@ -0,0 +1,14 @@
+"""
+Step 1: Copy the following code into Replit.
+
+Step 2: Add a line of code (outside of the class) to create the linked list 4 -> 3 -> 2 in a single assignment statement.
+"""
+class Node:
+	def __init__(self, value, next=None):
+		self.value = value
+		self.next = next
+
+head = Node(4, Node(3, Node(2)))
+print(head.value)
+print(head.next.value)
+print(head.next.next.value)
