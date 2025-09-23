7ded9ebcd18fac5023972b1eb6b9079d070fac77
Sushant Aryal <sushantaryal05@gmail.com>
2025-09-23 10:33:01 -0500

    acronym

---
+++ tip102-4a/1 - strings/acronym.py
@@ -0,0 +1,25 @@
+"""
+Given an array of strings words and a string s, implement a function is_acronym() that returns True if s is an 
+acronym of words and returns False otherwise.
+
+The string s is considered an acronym of words if it can be formed by concatenating the first character of
+ each string in words in order. For example, "pb" can be formed from ["pooh"", "bear"], but it can't be formed 
+ from ["bear", "pooh"]
+"""
+def is_acronym(words, s):
+    acronym = []
+    for word in words:
+        word.split()
+        acronym.append(word[0])
+
+    #print("".join(acronym))
+    
+    return "".join(acronym) == s
+
+#test case
+words = ["christopher", "robin", "milne"]
+s = "crm"
+print(is_acronym(words, s))
+
+
+        
